0ec4ba2124ad4e6f52dcef356801789cab40c548
CsarrrX <cesarpeam@proton.me>
2025-05-30 16:42:40 -0600

    quite configuraciones de rust-analyzer

--- lua/cesarx/plugins/rustaceanvim.lua
+++ lua/cesarx/plugins/rustaceanvim.lua
@@ -1,6 +1,6 @@
 return {
   'mrcjkb/rustaceanvim',
-  version = '^4',
+  version = '^4', 
   ft = { 'rust' },
   config = function()
     vim.g.rustaceanvim = {
@@ -8,18 +8,6 @@ return {
         on_attach = function(_, bufnr)
           -- Configura tus mappings de LSP aquí si usas alguno
         end,
-        default_settings = {
-          ['rust-analyzer'] = {
-            checkOnSave = true, -- Usa el comando por defecto del proyecto (cargo check/clippy)
-            diagnostics = {
-              enable = true,
-              disabled = { "unresolved-proc-macro" }, -- opcional
-            },
-            cargo = {
-              allFeatures = true,
-            },
-          },
-        },
       },
     }
   end,
